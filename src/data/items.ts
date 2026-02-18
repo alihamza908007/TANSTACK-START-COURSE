31b8ce8721aa8939b8157cddbd83f9f4803e513d
alihamza908007 <alihamza908007@gmail.com>
2026-02-18 13:07:27 +0500

    modified:   prisma/schema.prisma 	modified:   src/data/items.ts 	modified:   src/generated/prisma/browser.ts 	modified:   src/generated/prisma/client.ts 	modified:   src/generated/prisma/commonInputTypes.ts 	modified:   src/generated/prisma/enums.ts 	modified:   src/generated/prisma/internal/class.ts 	modified:   src/generated/prisma/internal/prismaNamespace.ts 	modified:   src/generated/prisma/internal/prismaNamespaceBrowser.ts 	modified:   src/generated/prisma/models.ts 	new file:   src/generated/prisma/models/SavedItem.ts 	deleted:    src/generated/prisma/models/Todo.ts 	modified:   src/generated/prisma/models/User.ts 	modified:   src/schemas/import.ts

--- src/data/items.ts
+++ src/data/items.ts
@@ -1,13 +1,56 @@
+import { prisma } from '@/db'
 import { firecrawl } from '@/lib/firecrawl'
-import { importSchema } from '@/schemas/import'
+import { extractSchema, importSchema } from '@/schemas/import'
 import { createServerFn } from '@tanstack/react-start'
+import z from 'zod'
+import { getSessionFn } from './session'
 
 export const scrapeUrlFn = createServerFn({ method: 'POST' })
   .inputValidator(importSchema)
   .handler(async ({ data }) => {
-    const result = await firecrawl.scrape(data.url, {
-      formats: ['markdown'],
-      onlyMainContent: true,
+    const user = await getSessionFn()
+    const item = await prisma.savedItem.create({
+      data: {
+        url: data.url,
+        userId: user?.user.id,
+        status: 'PROCESSING',
+      },
     })
-    console.log(result)
+    try {
+      const result = await firecrawl.scrape(data.url, {
+        formats: [
+          'markdown',
+          {
+            type: 'json',
+            schema: extractSchema,
+          },
+        ],
+        onlyMainContent: true,
+      })
+      const jsonData = result.json as z.infer<typeof extractSchema>
+      const updatedItem = await prisma.savedItem.update({
+        where: {
+          id: item.id,
+        },
+        data: {
+          title: result.metadata?.title || null,
+          content: result.markdown || null,
+          ogImage: result.metadata?.ogImage || null,
+          author: jsonData.author || null,
+          publishedAt: jsonData.publishedAt || null,
+          status: 'COMPLETED',
+        },
+      })
+      return updatedItem
+    } catch {
+      const failedItem = await prisma.savedItem.update({
+        where: {
+          id: item.id,
+        },
+        data: {
+          status: 'FAILED',
+        },
+      })
+      return failedItem
+    }
   })
